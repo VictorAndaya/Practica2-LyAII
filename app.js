b4742e99d55f13d23bc61628c3278c6d33578f2b
VictorAndaya <andayatabarez@gmail.com>
2020-05-19 04:01:06 -0500

    Validacion de Nc

--- app.js
+++ app.js
@@ -2,39 +2,61 @@
 //var futureBlockDate = new Date(); 
 //futureBlockDate.setDate(currentDate.getDate() + 30); 
 
-
 const input = document.querySelector('.NC');
-const nombre = document.querySelector('');
-const apePat = document.querySelector('');
+const Name = document.querySelector('#Nombre');
+const Boton = document.getElementById('button-a');
+const message = document.querySelector('invalid-feedback');
 
-
-input.addEventListener('change', validate);
+input.addEventListener('change', validateNc);
+Name.addEventListener('keypress',validateName);
 
 function fecha() {
     var inicio = document.getElementById('fechaIni').value;
     var finalq = document.getElementById('fechaTer').value;
-
-
     var primera = Date.parse(inicio);
     var segunda = Date.parse(finalq);
+    var Nombre = document.getElementById("Nombre").value;
+
+    if(Nombre == ""){
+        alert();
+    }
 
     if (primera == segunda) {
         alert("El campo fecha de inicio no puede ser igual a la fecha de termino");
+        Bien = false;
     } else if (primera > segunda) {
         alert("El campo fecha de termino no puede ser menor a la fecha de inicio");
+        Bien = false;
+    }
+
+
+}
+
+function validateNc() {
+    var NC = document.getElementById("NC").value
+    if(NC.match(/(0|1|9)\d(09)[0-4]\d\d\d+$/ || /\s+$/.test(NC)) == null){
+        input.classList.add('is-invalid');
+        Boton.disabled = true;
+    }else{
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+        Boton.disabled = false;
     }
     
 }
 
-function validate() {
+function check(e) {
+    tecla = (document.all) ? e.keyCode : e.which;
 
-    var NC = document.getElementById("NC").value
-    if(NC.match(/(0|1|9)\d(09)[0-4]\d\d\d$/) == null || NC == ""){
-        input.classList.add('is-invalid');
-    }else{
-        input.classList.remove('is-invalid');
-        input.classList.add('is-valid');
+    //Tecla de retroceso para borrar, siempre la permite
+    if (tecla == 8) {
+        return true;
     }
+
+    // Patron de entrada, en este caso solo acepta numeros y letras
+    patron = /[A-Za-z\s]/;
+    tecla_final = String.fromCharCode(tecla);
+    return patron.test(tecla_final);
 }
 
 
