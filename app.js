041004225cdd3b23ab1a2a7abf6611e260842013
VictorAndaya <andayatabarez@gmail.com>
2020-05-19 04:46:42 -0500

    validaciones

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
 //futureBlockDate.setDate(currentDate.getDate() + 30); 
 
 const input = document.querySelector('.NC');
-const Name = document.querySelector('#Nombre');
+const Name = document.querySelector('.datos');
 const Boton = document.getElementById('button-a');
 const message = document.querySelector('invalid-feedback');
 
 input.addEventListener('change', validateNc);
-Name.addEventListener('keypress',validateName);
+Name.addEventListener('change',fecha);
+
 
 function fecha() {
     var inicio = document.getElementById('fechaIni').value;
@@ -16,17 +17,21 @@ function fecha() {
     var primera = Date.parse(inicio);
     var segunda = Date.parse(finalq);
     var Nombre = document.getElementById("Nombre").value;
+    var ApePat = document.getElementById("ApePat").value;
 
-    if(Nombre == ""){
-        alert();
+    if(/\s|\s$/.test(Nombre)){
+        Name.classList.add('is-invalid');
+        Boton.disabled = true;
+    }else{
+        Name.classList.remove('is-invalid');
+        Name.classList.add('is-valid');
+        Boton.disabled = false;
     }
 
     if (primera == segunda) {
         alert("El campo fecha de inicio no puede ser igual a la fecha de termino");
-        Bien = false;
     } else if (primera > segunda) {
         alert("El campo fecha de termino no puede ser menor a la fecha de inicio");
-        Bien = false;
     }
 
 
@@ -34,7 +39,7 @@ function fecha() {
 
 function validateNc() {
     var NC = document.getElementById("NC").value
-    if(NC.match(/(0|1|9)\d(09)[0-4]\d\d\d+$/ || /\s+$/.test(NC)) == null){
+    if(NC.match(/(0|1|9)\d(09)[0-4]\d\d\d$/ || /\s+$/.test(NC)) == null){
         input.classList.add('is-invalid');
         Boton.disabled = true;
     }else{
@@ -54,7 +59,21 @@ function check(e) {
     }
 
     // Patron de entrada, en este caso solo acepta numeros y letras
-    patron = /[A-Za-z\s]/;
+    patron = /[\SA-Za-z]|[A-Za-z\s]/;
+    tecla_final = String.fromCharCode(tecla);
+    return patron.test(tecla_final);
+}
+
+function only(e) {
+    tecla = (document.all) ? e.keyCode : e.which;
+
+    //Tecla de retroceso para borrar, siempre la permite
+    if (tecla == 8) {
+        return true;
+    }
+
+    // Patron de entrada, en este caso solo acepta numeros y letras
+    patron = /[0-9]/;
     tecla_final = String.fromCharCode(tecla);
     return patron.test(tecla_final);
 }
